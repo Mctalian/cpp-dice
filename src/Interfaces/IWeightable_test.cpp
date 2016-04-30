2fc73bf83b492778c5f2cf161cd868cbef38b791
Robert <r-anderson.9@onu.edu>
2016-04-30 10:53:17 -0600

    Adds tests for invalid side and weight amounts

--- src/Interfaces/IWeightable_test.cpp
+++ src/Interfaces/IWeightable_test.cpp
@@ -3,12 +3,42 @@
 
 // Tests IWeightable.
 
-// Checks default initialization
+// Should initialize unweighted
 TEST(IWeightableTest, Unweighted) {
     IWeightable w1;
-    int amnt = w1.getLoadAmount();
+    int amnt;
+    int side;
+
+    amnt = w1.getLoadAmount();
     ASSERT_EQ(amnt, 1);
 
-    int side = w1.getLoadedSide();
+    side = w1.getLoadedSide();
+    ASSERT_EQ(side, 1);
+}
+
+// Should disallow invalid weights
+TEST(IWeightableTest, InvalidWeight) {
+    IWeightable w2;
+    int amnt;
+
+    w2.setLoadAmount(0);
+    amnt = w2.getLoadAmount();
+    ASSERT_NE(amnt, 0);
+    ASSERT_EQ(amnt, 1);
+}
+
+// Should disallow invalid sides
+TEST(IWeightableTest, InvalidSide) {
+    IWeightable w3;
+    int side;
+
+    w3.setLoadedSide(0);
+    side = w3.getLoadedSide();
+    ASSERT_NE(side, 0);
+    ASSERT_EQ(side, 1);
+
+    w3.setLoadedSide(7);
+    side = w3.getLoadedSide();
+    ASSERT_NE(side, 7);
     ASSERT_EQ(side, 1);
 }
